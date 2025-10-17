3defe43e185a8b93b9fefcbb04f0509b70f7a64c
berrypro08 <berrypro08@gmail.com>
2025-10-18 07:25:45 +0800

    PRAK204-2510817110012-Muhammad Dani Maulana.py

--- PRAK204-2510817110012-Muhammad Dani Maulana.py
+++ PRAK204-2510817110012-Muhammad Dani Maulana.py
@@ -1,4 +1,3 @@
-import math
 r = float(input("Masukkan Jari-Jari: "))
 h = float(input("Masukkan Tinggi: "))
 V = (22 * pow(r,2) * h)/7
@@ -7,4 +6,5 @@ K = 2*22*r/7
 
 print("%.2f"%V)
 print("%.2f"%L)
+
 print("%.2f"%K)
